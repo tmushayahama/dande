a285cd4abc406a244e8723399692d3dbcf216529
tmushayahama <tmushayahama@gmail.com>
2013-01-02 13:21:03 -0500

    Lets get started

---
+++ application/views/welcome_message.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to Dande</title>
+</head>
+<body>
+
+<div>
+	<h1>Dande</h1>
+	<div id="menu">
+		<ul>
+			<li>Home</li>
+			<li>Sign In</li>
+			<li>Contact Us</li>
+			<li>About</li>
+			<li>Help</li>
+		</ul>
+	</div>
+</div>
+
+</body>
+</html>
